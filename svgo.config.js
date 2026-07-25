edbe081d361ce4678d3bf1ee7591c4d0b9d83f00
Mamdouh Aboammar <mamdouhfces1997@gmail.com>
2026-07-25 18:23:21 +0300

    style: apply SVGO optimization preserving contract rules

--- svgo.config.js
+++ svgo.config.js
@@ -6,9 +6,6 @@ export default {
       name: "preset-default",
       params: {
         overrides: {
-          removeViewBox: false,
-          removeTitle: false,
-          removeDesc: false,
           cleanupIds: false,
           collapseGroups: false,
           removeUnknownsAndDefaults: false,
